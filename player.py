46fb8aa25244efa94329d75e4271d802a78b040d
landonharney19 <163608086+landonharney19@users.noreply.github.com>
2024-03-18 09:01:43 -0400

    Update player.py

--- player.py
+++ player.py
@@ -1,10 +1,19 @@
-import pygame
+import pygame as pg
 
 SCREEN_HEIGHT = 600
 SCREEN_WIDTH = 1000
 
-class Player():
-  speed = 5
+class Player(pg.sprite.Sprite):
+  def __init__(self, location=(50, 300)):
+        super().__init__()
+        self.speed = 10
+        self.size = 1.2
+        displaySize = (round(50 * self.size), round(50 * self.size))
+        self.score = 0
+
+        self.surf = pg.surface.Surface(displaySize) # replace w/ scaled image
+        self.surf.fill((0, 0, 0))
+        self.rect = self.surf.get_rect(center=location)
 
   def update(self, pressedKeys):
     if keypress[K_UP]:
