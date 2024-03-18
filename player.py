fcbb04ccf5f86cbf3666de1be71cb5982f30c29f
landonharney19 <163608086+landonharney19@users.noreply.github.com>
2024-03-17 23:35:42 -0400

    Update player.py

--- player.py
+++ player.py
@@ -6,5 +6,17 @@ SCREEN_WIDTH = 600
 class player():
 
   def update(self, pressedKeys):
+    if keypress[K_UP]:
+        self.rect.move_ip(-self.speed, 0)
+    if keypress[K_DOWN]:
+        self.rect.move_ip(self.speed, 0)
+    if self.rect.left < 0:
+        self.rect.left = 0
+    if self.rect.right > SCREEN_WIDTH:
+        self.rect.right = SCREEN_WIDTH
+    if self.rect.top <= 0:
+        self.rect.top = 0
+    if self.rect.bottom >= SCREEN_HEIGHT:
+        self.rect.bottom = SCREEN_HEIGHT
   
   
