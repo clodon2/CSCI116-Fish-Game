1bc602e6859452cf29a3b0a7e07858006c61f826
AustinA60 <128769804+AustinA60@users.noreply.github.com>
2024-03-18 21:48:39 -0400

    Last version
    Could not figure out the delete portion of this

--- ScoreBoard.py
+++ ScoreBoard.py
@@ -1,79 +1,90 @@
 class ScoreBoard (object):
     
-    def __init__(self,score,time):
-#Used to hold player score 
-        self.score= int(score)
-#Used to hold total game time
-        self.time=  int(time)
+    def __init__(self):
+
 #State of the list 
         self.List_Is_Full= None
 #A list to store our scores
-        self.OurList=[1,1,1,1,1,1,1,1,1]
-       
-    def main(self):
-        
-        self.List_Check()
-#These will all be method calls to handle differant states (like an enum in C#)
+        self.OurList=[]
+
+    def AddScore(self, score, time, name):
+# changes the data type of the variables
+        score = int(score)
+        time = str(time)
+        name = str(name)
+#alias
+        LS = self.OurList
+#checks to see what the state of the list 
+        if len(LS) < 10:
+
+            self.List_Is_Full = False 
+
+        elif len(LS) == 0:
+
+            self.List_Is_Full = None
+
+        elif len(LS) >= 10:
+
+            self.List_Is_Full = True
 
 
         if self.List_Is_Full == True:
-        #Run remove from list script 
+#remove score if overflowing 
             self.RemoveScore()
-        elif self.List_Is_Full == False:
-        #Add score to list 
-            self.AddScore(self.score,self.time)
-        elif self.List_Is_Full == None: 
-        #Create the list and add this entry 
-            self.AddScore(self.score,self.time)
-
-    def AddScore(self,score,time):
-        #Will add the score to the list calling on Place Score to determine the index to place it at.
-        print(str(self.time))
-        print(str(self.score))
-        self.PlaceScore()
-
-    def RemoveScore(self):
-        print("list is full removing score")
-
-
-
-    def PlaceScore(self):
-        #debug
-        return(print("Place Score Called"))
-
-
-    def List_Check(self):
-        OurList= self.OurList
-        List_Is_Full=self.List_Is_Full
-
-        print(List_Is_Full)
-        if len(OurList) == 0 :
-            self.List_Is_Full = None
-            #debug
-            print("List is not created ")
-
-        elif len(OurList)< 10 and len(OurList) !=0 :
-            self.List_Is_Full= False
-            #debug
-            print("List is not full ")
-
-        elif len(OurList) >9:
-            self.List_Is_Full= True
-            #debug
-            print("List is full")
 
         else:
-            print("unknown exception occured")
-Example=ScoreBoard(100,15)
-Example.main()
+
+            if self.List_Is_Full is None:
+                self.OurList.append((score, time, name))
+                self.OurList.sort(reverse=True)
+
+            else:
+
+                inserted_in_list = False
+# Insert the score at the appropriate position 
+                for i in range(len(self.OurList)):
+
+                    if score >= self.OurList[i][0]:  
+
+                        self.OurList.insert(i, (score, time, name))
+
+                        inserted_in_list = True
+
+                        break
+# Append the score if it's not already inserted 
+                if not inserted_in_list and (score, time, name) not in self.OurList:
+
+                    self.OurList.append((score, time, name))
+
+        return self.OurList
+                
+
+
+
+        
+
+    def RemoveScore(self):
+        pass
+        
+        # could not figure this out 
+
   
-#to this point the program can do the following
-#1 it can determine how many items are stored in a list
-#2.it can call the correct function to do the correct operation
-#3.it can set the instance variable to the correct state
+            
+            
+    
 
 
-#I need to do the following:
-#1.recive input from the game class
-#2. Create the logic to place the socre in the correct position
-#3.Evaluate the and place the score in the correct place 
+Example=ScoreBoard()
+Example.AddScore(200,15,"Austin")
+Example.AddScore(150,15,"Austin")
+Example.AddScore(225,15,"Austin")
+Example.AddScore(145,15,"Austin")
+Example.AddScore(256,15,"Austin")
+Example.AddScore(2456,15,"Austin")
+Example.AddScore(760,15,"Austin")
+Example.AddScore(100,15,"Austin")
+Example.AddScore(230,15,"Austin")
+Example.AddScore(204,15,"Austin")
+Example.AddScore(300,15,"Austin")
+print(Example.OurList)
+
